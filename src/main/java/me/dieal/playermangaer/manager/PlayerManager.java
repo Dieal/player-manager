5b549d626e5879e0c6c5c669dbd021ca62d06727
Dieal <diealblack2005@gmail.com>
2023-05-19 16:18:02 +0200

    Added player manager class, which manages online players (add / removal / update)

---
+++ src/main/java/me/dieal/playermangaer/manager/PlayerManager.java
@@ -0,0 +1,70 @@
+package me.dieal.playermangaer.manager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class PlayerManager {
+
+    private ArrayList<UUID> onlinePlayers;
+
+    // Constructor
+    public PlayerManager () {
+        updateOnlinePlayers();
+    }
+
+    private ArrayList<UUID> getOnlinePlayers () {
+
+        ArrayList<UUID> users = new ArrayList<>() ;
+
+        for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+            users.add(p.getUniqueId());
+        }
+
+        return users;
+
+    }
+
+    // Getters
+    public ArrayList<UUID> players () {
+        return new ArrayList<UUID>(onlinePlayers);
+    }
+
+    // Add player
+    public void addPlayer (Player p) {
+
+        if (p == null) {
+            return;
+        }
+
+        if (!p.isOnline()) {
+            return;
+        }
+
+        onlinePlayers.add(p.getUniqueId());
+
+    }
+
+    // Remove player
+    public void removePlayer (Player p) {
+
+        if (p == null) {
+            return;
+        }
+
+        if (onlinePlayers.size() == 0 || !onlinePlayers.contains(p.getUniqueId())) {
+            return;
+        }
+
+        onlinePlayers.remove(p.getUniqueId());
+
+    }
+
+    // Updates player list
+    public void updateOnlinePlayers () {
+        onlinePlayers = getOnlinePlayers();
+    }
+
+}
