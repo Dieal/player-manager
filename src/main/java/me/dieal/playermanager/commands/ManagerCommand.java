874443dee3b35130ad55aa4f2859e0bbcce35168
Dieal <diealblack2005@gmail.com>
2023-05-19 17:13:14 +0200

    Added '/manager' command

---
+++ src/main/java/me/dieal/playermanager/commands/ManagerCommand.java
@@ -0,0 +1,35 @@
+package me.dieal.playermanager.commands;
+
+import me.dieal.playermanager.manager.PlayerManager;
+import me.dieal.playermanager.manager.gui.OnlinePlayersMenu;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ManagerCommand implements CommandExecutor {
+
+    public PlayerManager manager;
+
+    public ManagerCommand (PlayerManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        if (!(commandSender instanceof Player)) {
+            return true;
+        }
+
+        if (strings.length != 0) {
+            return true;
+        }
+
+        Player player = (Player) commandSender;
+        player.openInventory(OnlinePlayersMenu.createMenu(player, manager));
+
+        return true;
+    }
+
+}
